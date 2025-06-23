6fede00ed67f25f4018652192311646d416cb469
Zeshan793z <zeshanahmed793@gmail.com>
2025-06-23 12:20:13 +0600

    Added  Course entity

---
+++ javacorner-admin/src/main/java/com/javacorner/admin/entity/Role.java
@@ -0,0 +1,20 @@
+package com.javacorner.admin.entity;
+
+import java.util.Objects;
+
+public class Role {
+    private Long roleId;
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(roleId, role.roleId) && Objects.equals(name, role.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roleId, name);
+    }
+}
