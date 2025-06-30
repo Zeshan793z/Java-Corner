748a345faa47c06a0158ce0811a201dd11d0e8c2
Zeshan793z <zeshanahmed793@gmail.com>
2025-06-30 12:23:15 +0600

    Added  Course entity

--- javacorner-admin/src/main/java/com/javacorner/admin/entity/Instructor.java
+++ javacorner-admin/src/main/java/com/javacorner/admin/entity/Instructor.java
@@ -1,17 +1,39 @@
 package com.javacorner.admin.entity;
 
+import jakarta.persistence.*;
+import jdk.jfr.Enabled;
+
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+@Entity
+@Table(name = "instructor")
+
 public class Instructor {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "instructor_id", nullable = false)
     private Long instructorId;
+
+    @Basic
+    @Column(name = "first_name", nullable = false, length = 45)
     private String firstName;
+
+    @Basic
+    @Column(name = "last_name", nullable = false, length = 45)
     private String lastName;
+
+    @Basic
+    @Column(name = "summary", nullable = false, length = 64)
     private String summary;
+
+    @OneToMany(mappedBy = "instructor", fetch = FetchType.LAZY)
     private Set <Course> courses = new HashSet<>();
 
+    @OneToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id",  nullable = false)
     private User user;
 
     @Override
